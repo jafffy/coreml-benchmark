b290b90a0ddeddf089fe17ae841c0a413172ad52
Jaewon Choi <jaewon-choi@yonsei.ac.kr>
2021-06-25 17:31:11 +0100

    Initial Commit

---
+++ coreml-benchmark/coreml_benchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreml_benchmarkApp.swift
+//  coreml-benchmark
+//
+//  Created by Jaewon Choi on 2021/06/25.
+//
+
+import SwiftUI
+
+@main
+struct coreml_benchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coreml-benchmarkTests/coreml_benchmarkTests.swift
@@ -0,0 +1,33 @@
+//
+//  coreml_benchmarkTests.swift
+//  coreml-benchmarkTests
+//
+//  Created by Jaewon Choi on 2021/06/25.
+//
+
+import XCTest
+@testable import coreml_benchmark
+
+class coreml_benchmarkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
