ce71776c32b9a5239a97a6ee6fac3e91a65d49be
Jaewon Choi <jaewon-choi@yonsei.ac.kr>
2021-07-11 23:21:30 +0100

    Benchmark 64x64x16

--- coreml-benchmark/ContentView.swift
+++ coreml-benchmark/ContentView.swift
@@ -6,11 +6,44 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear {
+                do {
+                    let model = try Siren(configuration: MLModelConfiguration())
+                    let hidden_size = 64
+                    let batch_size = 16
+                    
+                    let m1 = try? MLMultiArray(shape: [batch_size, 2, hidden_size] as [NSNumber], dataType: .float)
+                    let m2 = try? MLMultiArray(shape: [batch_size, hidden_size, hidden_size] as [NSNumber], dataType: .float)
+                    let m3 = try? MLMultiArray(shape: [batch_size, hidden_size, 3] as [NSNumber], dataType: .float)
+                    
+                    let b1 = try? MLMultiArray(shape: [batch_size, 1, hidden_size] as [NSNumber], dataType: .float)
+                    let b2 = try? MLMultiArray(shape: [batch_size, 1, hidden_size] as [NSNumber], dataType: .float)
+                    let b3 = try? MLMultiArray(shape: [batch_size, 1, 3] as [NSNumber], dataType: .float)
+                    
+                    let start = DispatchTime.now()
+                    
+                    for index in 1...1000 {
+                        guard (try? model.prediction(input: SirenInput(m1: m1!, b1: b1!, m2: m2!, b2: b2!, m3: m3!, b3: b3!))) != nil else {
+                            fatalError("Unexpected runtime error.")
+                        }
+                    }
+                    
+                    let end = DispatchTime.now()
+                    
+                    let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds // <<<<< Difference in nano seconds (UInt64)
+                    let timeInterval = Double(nanoTime) / 1_000_000_000 // Technically could overflow for long running tests
+
+                    print("Time to evaluate: \(timeInterval) seconds")
+                } catch {
+                    fatalError("Model initialization failed.")
+                }
+            }
     }
 }
 
